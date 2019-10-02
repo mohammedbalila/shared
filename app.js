d2bbcb4d11046a6380513931645b5a2436d07188
= <=>
2019-10-02 06:36:37 +0200

    fixed styles

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-const app = require("express")();
+const express = require("express");
 const path = require('path')
 const socket = require("socket.io");
 const compression = require('compression')
+const app = express();
 
-app.get('/',(req,res)=>{
-res.render('messages')
+app.get('/', (req, res) => {
+    res.render('messages')
 })
 
 
 const port = process.env.PORT || 3000
 
-app
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .use(compression())
-    .use('/assests', require("express").static(path.join(__dirname, 'assests')))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(compression());
+app.use('/assets', express.static("assets"));
 
-    let server = app.listen(port)
+const server = app.listen(port, () => {
+    console.log(`Running on ${port}`)
+});
 
 // Socket setup & pass server
-var io = socket(server);
+const io = socket(server);
 io.on('connection', (socket) => {
-
-    console.log('made socket connection', socket.id);
-
     // Handle chat event
-    socket.on('chat', function(data){
+    socket.on('chat', function (data) {
         // console.log(data);
         io.sockets.emit('chat', data);
     });
 
     // Handle typing event
-    socket.on('typing', function(data){
+    socket.on('typing', function (data) {
         socket.broadcast.emit('typing', data);
     });
 
